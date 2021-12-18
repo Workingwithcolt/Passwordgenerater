874891f3938fa204a929996f92068376791fd96f
Chetan <mohitechetan2019@gmail.com>
2021-12-18 21:22:40 +0530

    This is the passwordgenerater

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+window = Tk()
+canvas = Canvas(width = 300,height = 300)
+home =  PhotoImage(file = "logo.png")
+
+canvas.create_image(80,100,image = home)
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window.mainloop()
